9ad88396879bfaf65c484f8fc3772dfc38b04ee6
bizothesup <Supercreator5>
2020-06-04 12:08:13 +0000

    login client conducteur

--- routes/users.js
+++ routes/users.js
@@ -39,9 +39,10 @@ router.post('/register',function (req,res,next) {
                    }else {
                        if(rows.length >0){
                            if(login_type != phone && rows['login_type']==login_type){
-                               res.send(JSON.stringify({success:false,message:"Social Login"}))
+                               var user = rows[0].reduce(mdp);
+                               res.send(JSON.stringify({success:false,message:"Social Login",etat:1,user: user}))
                            }else{
-                               res.send(JSON.stringify({success:false,message:"Phone existe Déjà ......."}))
+                               res.send(JSON.stringify({success:false,message:"Phone existe Déjà .......",etat:2}))
                            }
                        }else {
                            mdp= bcrypt.hashSync(password, 10);
@@ -55,7 +56,7 @@ router.post('/register',function (req,res,next) {
                                        res.send(JSON.stringify({success:false,message:err.message}))
                                    }else{
                                        if(rows.affectedRows > 0){
-                                           res.send(JSON.stringify({success:true,message:"Success"}))
+                                           res.send(JSON.stringify({success:true,message:"Success",etat:1,user:rows[0]}))
                                        }else {
                                            res.send(JSON.stringify({success:false,message:"Vide"}))
                                        }
@@ -74,15 +75,15 @@ router.post('/register',function (req,res,next) {
                       res.send(JSON.stringify({success:false,message:err.message}))
                   }else {
                       if(rows.length >0){
-                          if(login_type != phone && rows['login_type']==login_type){
-                              res.send(JSON.stringify({success:false,message:"Social Login"}))
+                          if(login_type != 'phone' && rows['login_type']==login_type){
+                              var user = rows[0].reduce(mdp);
+                              res.send(JSON.stringify({success:false,message:"Social Login",etat:1,user: user}))
                           }else{
-                              res.send(JSON.stringify({success:false,message:"Phone existe Déjà ......."}))
+                              res.send(JSON.stringify({success:false,message:"Phone existe Déjà .......",etat:2}))
                           }
                       }else {
                           mdp= bcrypt.hashSync(password, 10);
 
-                          console.log(mdp);
                            con.query('insert into conducteur(online,nom,prenom,phone,mdp,statut,login_type,tonotify,creer,statut_licence,statut_nic,statut_vehicule,email) ' +
                               'values (?,?,?,?,?,?,?,?,?,?,?,?,?)',['yes',lastname,firstname,phone,mdp,'no',login_type,tonotify,date_heure,'no','no','no',email],
                               function (err,rows,field) {
@@ -91,7 +92,6 @@ router.post('/register',function (req,res,next) {
                                       res.send(JSON.stringify({success:false,message:err.message}))
                                   }else{
                                       if(rows.affectedRows > 0){
-                                          res.send(JSON.stringify({success:true,message:rows.insertId}))
 
                                           con.query('select * from conducteur where id=?',[rows.insertId],function (err,rows,field) {
 
@@ -100,7 +100,7 @@ router.post('/register',function (req,res,next) {
                                                   res.send(JSON.stringify({success:false,message:err.message}))
                                               }else{
                                                   if(rows.length > 0){
-                                                      res.send(JSON.stringify({success:true,message:"Success"}))
+                                                      res.send(JSON.stringify({success:true,message:"Success",etat:1,user:rows[0]}))
                                                   }
                                               }
 
@@ -201,4 +201,107 @@ router.post('/login',function (req,res,next) {
     }
 
 });
+
+router.post('/conducteur',function (req,res,next) {
+    if(req.body.key == API_Key) {
+        var phone = req.body.phone;
+        var password = req.body.password;
+        var user;
+        req.getConnection(function (error,con) {
+            asyncLib.waterfall([
+                function(done){
+                    con.query('select * from conducteur where phone=?',[phone],function (err,rows,field) {
+                        if(err){
+                            res.status(500);
+                            res.send(JSON.stringify({success:false,etat:0}));
+                        }else {
+                            if(rows.length > 0){
+                                user=rows[0];
+                                done(null,user);
+                            }else{
+                                res.send(JSON.stringify({success:false,etat:0}));
+                            }
+                        }
+                    })
+                },
+                function (user,done) {
+                    con.query("select * from conducteur where phone=? and statut='yes'",[user.phone],function (err,rows,field) {
+                        if(err){
+                            res.status(500);
+                            res.send(JSON.stringify({success:false,message:err.message,etat:3}))
+                        }else {
+                            if(rows.length > 0){
+                                user =rows[0];
+                                done(null,user)
+                            }else{
+                                res.send(JSON.stringify({success:false,etat:3}));
+                            }
+                        }
+                    });
+                },function(user, done){
+                    if(user){
+                        if(bcrypt.compare(password, user.mdp)){
+                            done(null,user)
+                        }else {
+                            res.send(JSON.stringify({success: false, message: "Mot de Passe Incorrect"}))
+                        }
+                    }
+                },function(user, done){
+                    if(user){
+                        user['user_cat'] = 'conducteur';
+                        user['online'] = '';
+                        var id_user = user.id;
+
+                        con.query("select * from currency where statut='yes' limit 1",function (err,rows,field) {
+                            if (rows.length > 0){
+                                user['currency']=rows[0].symbole;
+                                done(null,user)
+                            }else {
+                                res.send(JSON.stringify({success:false,message:"monnaie not found"}))
+                            }
+                        });
+                    }
+                },function(user, done){
+                    if(user){
+                        con.query("select * from country where statut='yes' limit 1",function (err,rows,field) {
+                            if (rows.length > 0){
+                                user['country']=rows[0].code;
+                                done(null,user)
+                            }else {
+                                res.send(JSON.stringify({success:false,message:"Pays not found"}))
+                            }
+                        })
+                    }
+                },
+                function(user, done){
+                    if(user){
+                        con.query("select * from vehicule where statut='yes' AND id_conducteur=?",[user.id],function (err,rows,field) {
+                            if (rows.length > 0){
+                                user['brand'] = rows[0].brand;
+                                user['model'] = rows[0].model;
+                                user['color'] = rows[0].color;
+                                user['numberplate'] = rows[0].numberplate;
+
+                                done(user)
+                            }else {
+                                res.send(JSON.stringify({success:false,message:"Voiture not found"}))
+                            }
+                        })
+                    }
+                }
+            ],function (user) {
+                if (user){
+                    res.send(JSON.stringify({status: 200,success:true,message:"success",etat:1,user:user}));
+                } else {
+                    res.send(JSON.stringify({success:false,etat:2}));
+                }
+            });
+        })
+
+    }else{
+        res.send(JSON.stringify(({success:false,message:"Wrong API KEY"})));
+    }
+
+});
+
 module.exports = router;
